cc05a604092cee962ef2805e30dd19dd4e1a67c8
Christophe Nshimiyimana <ragonartsofficial@gmail.com>
2023-11-29 23:48:53 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById('next').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList').scrollLeft += widthItem;
+}
+document.getElementById('prev').onclick = function(){
+    const widthItem = document.querySelector('.item').offsetWidth;
+    document.getElementById('formList').scrollLeft -= widthItem;
+}
